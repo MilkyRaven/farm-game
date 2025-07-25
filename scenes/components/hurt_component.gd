36b132e10c7140c862437da06565f16b493b615c
Milky Raven <heartonllamas@gmail.com>
2025-07-25 12:18:15 +0200

    create mineable rocks

--- scenes/components/hurt_component.gd
+++ scenes/components/hurt_component.gd
@@ -5,6 +5,8 @@ extends Area2D
 
 #2- declaramos una variable exportada, lo que permtie editarla desde el editor
 #tool queda inicializada como none
+#tool es la herramienta que necesita el componente
+#HurtComponent para reaccionar
 @export var tool: DataTypes.Tools = DataTypes.Tools.None
 
 #declaramos una señal personalizada que puedo emitir en mi código cuando el componente sufra daño
@@ -13,6 +15,11 @@ signal hurt
 
 func _on_area_entered(area: Area2D) -> void:
 	var hit_component = area as HitComponent
-	
+	print("user tool", hit_component.current_tool)
+
 	if tool == hit_component.current_tool:
-			hurt.emit(hit_component.hit_damage)
+		print("[DEBUG] Tool matched. Emitting signal with:", hit_component.hit_damage)
+		hurt.emit(hit_component.hit_damage)
+	else:
+		print("[DEBUG] Tool did not match. My tool:", tool, "Hit tool:", hit_component.current_tool)
+	
